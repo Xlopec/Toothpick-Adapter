6f4784943c9a2daff623303e6a0694e8623298b3
Max Oliynick <maksym.oliinyk@ctdev.io>
2018-11-13 15:20:12 +0200

    fixed bug when fragment did not release injections when popping from backstack

--- src/main/java/com/oliynick/max/toothpickadapter/target/InjectableV4Fragment.kt
+++ src/main/java/com/oliynick/max/toothpickadapter/target/InjectableV4Fragment.kt
@@ -1,5 +1,6 @@
 package com.oliynick.max.toothpickadapter.target
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -7,6 +8,7 @@ import com.oliynick.max.toothpickadapter.misc.*
 import toothpick.Scope
 import toothpick.Toothpick
 import toothpick.config.Module
+import java.io.PrintWriter
 
 abstract class InjectableV4Fragment protected constructor(private inline val provider: (InjectableV4Fragment, Bundle?) -> Array<out Module> = { _, _ -> emptyArray() }) : Fragment(),
         ComponentHolder {
@@ -47,8 +49,18 @@ abstract class InjectableV4Fragment protected constructor(private inline val pro
     }
 
     override fun onDestroy() {
+
+        dump("this $this", null, PrintWriter(System.out, true), null)
+
         try {
-            if (activity!!.isFinishing || !isOnSaveStateCalled) {
+            val shouldReleaseInjections = requireActivity().willNotBeReCreated()
+                    || (requireActivity().isDestroyingBecauseOfConfigChanges() && !isOnSaveStateCalled)
+            // We don't keep fragment injections when:
+            // * activity is finishing
+            // * activity is being destroyed because of non-config changes
+            // * activity is being destroyed because of config changes,
+            // BUT onSaveInstanceState haven't been called
+            if (shouldReleaseInjections) {
                 // fragment won't be recreated later
                 // destroy to avoid memory leaks
                 Toothpick.closeScope(key)
@@ -60,6 +72,14 @@ abstract class InjectableV4Fragment protected constructor(private inline val pro
             isOnSaveStateCalled = false
             super.onDestroy()
         }
+    }
+
+    private fun Activity.willNotBeReCreated(): Boolean {
+        return isFinishing || !isDestroyingBecauseOfConfigChanges()
+    }
+
+    private fun Activity.isDestroyingBecauseOfConfigChanges(): Boolean {
+        return changingConfigurations != 0
     }
 
     protected open fun onPostInject(key: Key, scope: Scope, modules: Array<out Module>, savedInstanceState: Bundle?) = Unit
